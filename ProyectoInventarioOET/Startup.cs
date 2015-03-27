2c228f6f9977469a6136bbdf73e02719cebe5917
fergo125 <ffmm.14@gmail.com>
2015-03-27 09:55:09 -0600

    Primer push, proyecto vacio

---
+++ ProyectoInventarioOET/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoInventarioOET.Startup))]
+namespace ProyectoInventarioOET
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
