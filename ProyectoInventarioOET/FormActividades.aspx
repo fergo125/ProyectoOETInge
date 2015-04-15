e4ba05158051b817d7693e84f1fd4f117a5f875b
alesk8400 <alesk8400@gmail.com>
2015-04-15 10:38:57 -0600

    Agregue el Form de Actividades

---
+++ ProyectoInventarioOET/FormActividades.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FormActividades.aspx.cs" Inherits="ProyectoInventarioOET.FormActividades" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <script type="text/javascript">
+    function showStuff(id) {
+        var estado = document.getElementById(id).style.display;
+        if (estado === 'none') {
+            document.getElementById(id).style.display = 'block'; //Color 7BC134
+        } else {
+            document.getElementById(id).style.display = 'none';
+        }
+    }
+    </script>
+
+    <br />
+    <!-- Label para desplegar mensajes -->
+    <div>
+        <div id="mensajeAlerta" class="alert alert-fozkr-error fade in" runat="server" style="margin-left: 70%;">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <strong>
+                <asp:Label ID="labelTipoAlerta" runat="server" Text="Alerta! "></asp:Label>
+            </strong>
+            <asp:Label ID="labelAlerta" runat="server" Text="Mensaje de alerta"></asp:Label>
+        </div>
+    </div>
+
+    <!-- Título del Form -->
+    <div>
+        <h2 id="Titulos" runat="server"> Actividades </h2>
+        <hr />
+    </div>
+
+    <!-- Cuerpo del Form -->
+    <button runat="server" onclick="showStuff('MainContent_FieldsetActividad');" id="botonAgregaractividades" class=" btn btn-info" type="button" style="float: left" > Nueva Actividad</button>
+    <button runat="server" onclick="showStuff('MainContent_FieldsetActividad');" id="botonModificacionactividades" class=" btn btn-info" type="button" style="float: left"><i></i> Modificar Actividad </button>
+    <button runat="server" onserverclick="Page_Load" id="botonConsultaactividades" class=" btn btn-info" type="button" style="float: left"><i></i>Consulta de Actividades </button>
+    <%--<button runat="server" onserverclick="botonRedireccionProductos_ServerClick" id="botonRedireccionProductos" class=" btn btn-primary" type="button" style="float:right" ><i></i> Productos</button>--%>
+
+
+    <br />
+    <br />
+
+         <!-- Fieldset para Actividades -->
+    <fieldset id= "FieldsetActividad" style="display:none" runat="server" class="fieldset">
+        <legend> Ingresar datos de nueva Actividad: </legend>
+    
+        <br />
+        <br />
+        <br />
+
+        <div class= "col-md-4">
+
+            <div class= "form-group">
+                <label for="inputNombreActividad" class= "control-label"> Nombre: </label>      
+                <input type="text" id= "inputNombreActividad" class= "form-control"><br>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="form-group">
+                <label for="inputDescripcionActividad" class= "control-label"> Descripción: </label>      
+                <input type="text" id= "inputDescripcionActividad" class="form-control"><br>
+            </div>
+
+        </div>
+
+        <div class="col-md-4">
+            <div class="form-group">
+                <label for="inputEstadoActividad" class= "control-label"> Estado: </label>      
+                <asp:DropDownList id="comboBoxEstadosActividades" runat="server" DataSourceID="" DataTextField="" DataValueField="" CssClass="form-control">
+                    <asp:ListItem Selected="True">Ninguno</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+
+        </div>
+
+        
+    </fieldset>
+
+
+</asp:Content>
---
+++ ProyectoInventarioOET/FormActividades.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ProyectoInventarioOET {
+    
+    
+    public partial class FormActividades {
+        
+        /// <summary>
+        /// Control mensajeAlerta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl mensajeAlerta;
+        
+        /// <summary>
+        /// Control labelTipoAlerta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label labelTipoAlerta;
+        
+        /// <summary>
+        /// Control labelAlerta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label labelAlerta;
+        
+        /// <summary>
+        /// Control Titulos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Titulos;
+        
+        /// <summary>
+        /// Control botonAgregaractividades.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton botonAgregaractividades;
+        
+        /// <summary>
+        /// Control botonModificacionactividades.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton botonModificacionactividades;
+        
+        /// <summary>
+        /// Control botonConsultaactividades.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton botonConsultaactividades;
+        
+        /// <summary>
+        /// Control FieldsetActividad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl FieldsetActividad;
+        
+        /// <summary>
+        /// Control comboBoxEstadosActividades.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList comboBoxEstadosActividades;
+    }
+}
