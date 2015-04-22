7a97cd16d272172e50ef10a4f7fc0ad77a7d8edb
Leonardo Villalobos <artist_artistian@hotmail.com>
2015-04-22 10:41:55 -0600

    Agregados prototipos de clases asociadas a controladora de datos generales.

---
+++ ProyectoInventarioOET/App_Code/ControladoraBDUnidades.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProyectoInventarioOET.DataSets;
+
+/*
+ * Controladora del conjunto de datos de las unidades métricas del sistema.
+ * Comunicación con la Base de Datos.
+ */
+namespace ProyectoInventarioOET.App_Code
+{
+    public class ControladoraBDUnidades
+    {
+        /*
+        DataSetUnidades adaptadorUnidades;
+
+        public ControladoraBDUnidades()
+        {
+            adaptadorUnidades = new DataSetUnidades();
+        }
+
+        public DataTable consultarUnidades()
+        {
+            DataTable resultado = new DataTable();
+            try
+            {
+                resultado = adaptadorUnidades.GetData().CopyToDataTable();
+            }
+            catch (Exception e)
+            {
+                resultado = null;
+            }
+            return resultado;
+        }
+        */
+    }
+}
