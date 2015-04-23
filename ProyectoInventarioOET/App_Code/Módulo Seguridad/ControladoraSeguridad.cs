fcfde38633b6ff37c1fea5cca795a2ef5b1d860b
Leonardo Villalobos <artist_artistian@hotmail.com>
2015-04-23 11:00:01 -0600

    Creada entidadUsuario e iniciado trabajo en la controladora de Seguridad

---
+++ ProyectoInventarioOET/App_Code/Módulo Seguridad/ControladoraSeguridad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace ProyectoInventarioOET.App_Code.Módulo_Seguridad
+{
+    public class ControladoraSeguridad
+    {
+        /*
+        public EntidadUsuario consultarUsuario(String nombre, String password)
+        {
+            return
+        }*/
+    }
+}
