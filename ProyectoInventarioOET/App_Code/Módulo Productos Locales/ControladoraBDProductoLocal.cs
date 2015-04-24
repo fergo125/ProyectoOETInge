6d6de13fb7717d2034020b5704974d55af150465
jk_darklord <juan-ca-pq@hotmail.com>
2015-04-24 17:44:48 -0600

    Controladoras para Blopa.

---
+++ ProyectoInventarioOET/App_Code/Módulo Productos Locales/ControladoraBDProductoLocal.cs
@@ -0,0 +1,109 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace ProyectoInventarioOET.App_Code.Módulo_ProductosLocales
+{
+    public class ControladoraBDProductosLocales : ControladoraBD
+    {
+        public EntidadProductoLocal consultarProductoLocal(String id)
+        {
+            DataTable resultado = new DataTable();
+            EntidadProductoLocal producConsultado = null;
+            Object[] datosConsultados = new Object[3];
+
+            try
+            {
+                OracleCommand command = conexionBD.CreateCommand();
+                command.CommandText = "SELECT * FROM INV_ACTIVIDAD WHERE INV_PRODUCTOS = '" + id + "'";
+                OracleDataReader reader = command.ExecuteReader();
+                resultado.Load(reader);
+
+                if (resultado.Rows.Count == 1)
+                {
+                    datosConsultados[0] = id;
+                    for (int i = 1; i < 3; i++)
+                    {
+                        datosConsultados[i] = resultado.Rows[0][i].ToString();
+                    }
+
+                    producConsultado = new EntidadProductoLocal(datosConsultados);
+                }
+            }
+            catch (Exception e) { }
+
+            return producConsultado;
+        }
+
+        public string[] insertarProductoLocal(EntidadProductoLocal productoLocal)
+        {
+            String[] res = new String[4];
+            res[3] = productoLocal.Codigo.ToString();
+            try
+            {
+                DataTable resultado = new DataTable();
+                OracleCommand command = conexionBD.CreateCommand();
+                command.CommandText = "INSERT INTO INV_PRODUCTOS (NOMBRE, CAT_CATEGORIA, CAT_UNIDADES, CODIGO, CODIGO_BARRAS, " +
+                " ESTACION, ESTADO, COSTO_COLONES, COSTO_DOLARES ) VALUES ('"
+                + productoLocal.Nombre + "','" + productoLocal.Categoria + "','"
+                + productoLocal.Unidades + "','" + productoLocal.Codigo + "','"
+                + productoLocal.CodigoDeBarras + "','" + productoLocal.Estado + "','"
+                + productoLocal.CostoColones + "','" + productoLocal.CostoDolares + "');";
+                OracleDataReader reader = command.ExecuteReader();
+
+                res[0] = "success";
+                res[1] = "Exito";
+                res[2] = "Producto Agregada";
+            }
+            catch (SqlException e)
+            {
+                // Como la llave es generada se puede volver a intentar
+                res[0] = "danger";
+                res[1] = "Fallo en la operacion";
+                res[2] = "Intente nuevamente";
+            }
+            return res;
+        }
+
+        public string[] modificarProductoLocal(EntidadProductoLocal bodegaVieja, EntidadProductoLocal bodegaNueva)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] desactivarProductoLocal(EntidadProductoLocal productoLocal)
+        {
+            String[] res = new String[4];
+            res[3] = productoLocal.Codigo.ToString();
+            try
+            {
+                DataTable resultado = new DataTable();
+                OracleCommand command = conexionBD.CreateCommand();
+                command.CommandText = "UPDATE INV_PRODUCTOS "
+                                      + "SET ESTADO = " + productoLocal.Estado.ToString()
+                                      + "WHERE INV_PRODUCTOS = " + productoLocal.Inv_Productos;
+                OracleDataReader reader = command.ExecuteReader();
+
+                res[0] = "success";
+                res[1] = "Exito";
+                res[2] = "Producto Desactivado";
+            }
+            catch (SqlException e)
+            {
+                // Como la llave es generada se puede volver a intentar
+                res[0] = "danger";
+                res[1] = "Fallo en la operacion";
+                res[2] = "Intente nuevamente";
+            }
+            return res;
+        }
+
+        internal static System.Data.DataTable consultarProductosLocales()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProyectoInventarioOET/App_Code/Módulo Productos Locales/ControladoraProductoLocal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace ProyectoInventarioOET.App_Code.Módulo_Productos_Locales
+{
+    public class ControladoraProductoLocal
+    {
+        private ControladoraBDProductoLocal controladoraBD;
+        private EntidadProductoLocal productoActual; 
+
+        public ControladoraProductoLocal()
+        {
+            controladoraBD = new ControladoraBDProductoLocal();
+        }
+
+
+        public EntidadProductoLocal consultarProductoLocal(String id)
+        {
+            productoActual = controladoraBD.consultarProductoLocal(id);
+            return productoActual;
+        }
+
+        public String[] insertarDatos(String nombre, String categoria, String unidades, String codigo, String codigoDeBarras, String estacion,
+            int estado, double costoColones, double costoDolares)
+        {
+            Object[] datosProductoLocal = new Object[9];
+            datosProductoLocal[0] = nombre;
+            datosProductoLocal[1] = categoria;
+            datosProductoLocal[2] = unidades;
+            datosProductoLocal[3] = codigo;
+            datosProductoLocal[4] = codigoDeBarras;
+            datosProductoLocal[5] = estacion;
+            datosProductoLocal[6] = estado;
+            datosProductoLocal[7] = costoColones;
+            datosProductoLocal[8] = costoColones;
+            EntidadProductoLocal productoLocal = new EntidadProductoLocal(datosProductoLocal);
+            return controladoraBD.insertarProductoLocal(productoLocal);
+        }
+
+        public String[] modificarDatos(EntidadProductoLocal productoLocalViejo, String nombreModificado, String categoriaModificado, String unidadesModificado, String codigoModificado,
+            String codigoDeBarrasModificado, String estacionModificado, int estadoModificado, double costoColonesModificado, double costoDolaresModificado)
+        {
+            Object[] datosProductoLocalModificado = new Object[9];
+            datosProductoLocalModificado[0] = nombreModificado;
+            datosProductoLocalModificado[1] = categoriaModificado;
+            datosProductoLocalModificado[2] = unidadesModificado;
+            datosProductoLocalModificado[3] = codigoModificado;
+            datosProductoLocalModificado[4] = codigoDeBarrasModificado;
+            datosProductoLocalModificado[5] = estacionModificado;
+            datosProductoLocalModificado[6] = estadoModificado;
+            datosProductoLocalModificado[7] = costoColonesModificado;
+            datosProductoLocalModificado[8] = costoColonesModificado;
+            EntidadProductoLocal productoLocalModificado = new EntidadProductoLocal(datosProductoLocalModificado);
+            return controladoraBD.modificarProductoLocal(productoLocalViejo, productoLocalModificado);
+        }
+
+        public String[] desactivarProductoLocal(EntidadProductoLocal productoLocal)
+        {
+            /*desactiva una bodega de la base de datos*/
+            return controladoraBD.desactivarProductoGlobal(productoLocal);
+        }
+
+        /*
+         * Método para cargar el grid
+         */
+        public DataTable consultarProductosLocales()
+        {
+            /*consulta la información de todas los productos globales*/
+            return ControladoraBDProductosGlobales.consultarProductosLocales();
+        }
+    }
+}
