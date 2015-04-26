7b5560d7321f570756d012ea3c2b9567cfb4a44e
Oscar Esquivel <oscar12esquivel@gmail.com>
2015-04-26 16:36:32 -0600

    Iniciada documentación de ControladoraDatosGenerales.cs

--- ProyectoInventarioOET/App_Code/ControladoraDatosGenerales.cs
+++ ProyectoInventarioOET/App_Code/ControladoraDatosGenerales.cs
@@ -4,30 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using ProyectoInventarioOET.Módulo_Bodegas;
-/*
- * Controladora de datos generales, utilizada entre varios módulos.
- * Utiliza patrón singleton.
- * Auspicia datos sobre estaciones, compañías, unidades métricas, y estados.
- * 
- * Al ser instanciada trae todos los datos desde la base de datos, almacenándolos en memoria.
- * Por esto, para que se actualicen es necesario cerrar el explorador y volver a abrirlo.
- * Se hizo de esta forma tomando en cuenta la invariabilidad de estos datos.
- */
 namespace ProyectoInventarioOET.App_Code
 {
+    /*
+     * Controladora de datos generales, utilizada entre varios módulos.
+     * Utiliza patrón singleton.
+     * Auspicia datos sobre estaciones, compañías, unidades métricas, estados, entre otros de interés general.
+     * 
+     * Al ser instanciada trae todos los datos desde la base de datos, almacenándolos en memoria.
+     * Por esto, para que se actualicen es necesario cerrar el explorador y volver a abrirlo.
+     * Se hizo de esta forma tomando en cuenta la invariabilidad de estos datos.
+     */
     public class ControladoraDatosGenerales
     {
-        private static ControladoraDatosGenerales instanciaSingleton = null;
-        private DataTable estados;
-        private DataTable estadosActividad;
-        private DataTable unidades;
-        private DataTable estaciones;
-        private DataTable anfitrionas;
-        private int tipoCambioCompra;
-        private int tipoCambioVenta;
-        private int impuesto;
-        private DataTable intenciones;
+        private static ControladoraDatosGenerales instanciaSingleton = null;    //Instancia que será usada por todos
+        private DataTable estadosActividad;                                     //???
+        private DataTable anfitrionas;                                          //Información de las compañías (OET/ESINTRO)
+        private DataTable intenciones;                                          //Posibles intenciones de uso para los productos
+        private DataTable estaciones;                                           //Estaciones de la OET existentes
+        private DataTable unidades;                                             //Posibles unidades métricas para los productos
+        private DataTable estados;                                              //Posibles estados para todas las entidades del sistema
+        private int tipoCambioCompra;                                           //Tipo de cambio colones a dólares
+        private int tipoCambioVenta;                                            //Tipo de cambio dólares a colones
+        private int impuesto;                                                   //Impuesto de venta (13%)
         
+        /*
+         * Método usado para instanciar el objeto singleton que será usado por todas las controladoras que lo necesiten.
+         * Sólo debe ser creada una vez por cada ejecución del sistema en cada servidor.
+         */
         public static ControladoraDatosGenerales Instanciar
         {
             get
@@ -38,74 +42,109 @@ namespace ProyectoInventarioOET.App_Code
             }
         }
 
+        /*
+         * Constructor, inicializa las controladoras de datos generales individualmente para servir de interfaz entre
+         * las mismas y las demás controladoras del sistema. A la vez carga las tablas de interés.
+         */
         private ControladoraDatosGenerales()
         {
+            ControladoraBDGeneral controladoraGeneral = new ControladoraBDGeneral();
             ControladoraBDEstados controladoraEstados = new ControladoraBDEstados();
+            ControladoraBDBodegas controladoraBDBodegas = new ControladoraBDBodegas();
             ControladoraBDUnidades controladoraUnidades = new ControladoraBDUnidades();
             ControladoraBDEstaciones controladoraEstaciones = new ControladoraBDEstaciones();
             ControladoraBDAnfitrionas controladoraAnfitriones = new ControladoraBDAnfitrionas();
-            ControladoraBDBodegas controladoraBDBodegas = new ControladoraBDBodegas();
-            ControladoraBDGeneral controladoraGeneral = new ControladoraBDGeneral();
 
             estados = controladoraEstados.consultarEstados();
-            estadosActividad = controladoraEstados.consultarEstadosActividad();
+            impuesto = controladoraGeneral.consultarImpuesto();
             unidades = controladoraUnidades.consultarUnidades();
             estaciones = controladoraEstaciones.consultarEstaciones();
+            intenciones = controladoraBDBodegas.consultarIntenciones();
             anfitrionas = controladoraAnfitriones.consultarAnfitriones();
-            impuesto = controladoraGeneral.consultarImpuesto();
+            estadosActividad = controladoraEstados.consultarEstadosActividad();
 
             DataTable tipoCambio = controladoraGeneral.consultarTipoCambio();
             tipoCambioCompra = Convert.ToInt32(tipoCambio.Rows[0][0]);
             tipoCambioVenta = Convert.ToInt32(tipoCambio.Rows[0][1]);
-            intenciones = controladoraBDBodegas.consultarIntenciones();
+
+            //TODO: borrar las instancias de controladorasBD aquí si ya no se volverán a utilizar
         }
 
+        /*
+         * ???
+         */
         public DataTable consultarEstados()
         {
             return estados;
         }
 
-        // Activo e inactivo
+        /*
+         * Retorna un subgrupo de los posibles estados: "Activo" e "Inactivo".
+         */
         public DataTable consultarEstadosActividad()
         {
             return estadosActividad;
         }
 
+        /*
+         * ???
+         */
         public DataTable consultarUnidades()
         {
             return unidades;
         }
 
+        /*
+         * ???
+         */
         public DataTable consultarEstaciones()
         {
             return estaciones;
         }
 
+        /*
+         * ???
+         */
         public DataTable consultarAnfitriones()
         {
             return anfitrionas;
         }
 
+        /*
+         * ???
+         */
         public DataTable consultarIntenciones()
         {
             return intenciones;
         }
 
+        /*
+         * ???
+         */
         public String traduccionEstado(int valor)
         {
             return estados.Rows[valor-1][0].ToString();
         }
 
+        /*
+         * ???
+         */
         public int impuestoVentas()
         {
             return impuesto;
         }
 
+        /*
+         * ???
+         */
         public int dolarCompra()
         {
             return tipoCambioCompra;
         }
 
+        /*
+         * ???
+         */
         public int dolarVenta()
         {
             return tipoCambioVenta;
