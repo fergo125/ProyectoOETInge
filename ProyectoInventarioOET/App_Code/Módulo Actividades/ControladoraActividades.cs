cc18f953949acf4a9e3b005d2bc6e7ea17aa287e
Oscar Esquivel <oscar12esquivel@gmail.com>
2015-04-26 16:49:05 -0600

    Iniciada documentación de ControladoraActividades.cs

--- ProyectoInventarioOET/App_Code/Módulo Actividades/ControladoraActividades.cs
+++ ProyectoInventarioOET/App_Code/Módulo Actividades/ControladoraActividades.cs
@@ -6,27 +6,35 @@ using System.Web;
 
 namespace ProyectoInventarioOET.Módulo_Actividades
 {
+    /*
+     * ???
+     */
     public class ControladoraActividades
     {
+        //Atributos
+        private ControladoraBDActividades controladoraBDActividades;    //???
 
- 
-        private ControladoraBDActividades controladoraBDActividades;
-
+        /*
+         * Constructor.
+         */
         public ControladoraActividades()
         {
             controladoraBDActividades = new ControladoraBDActividades();
         }
 
-
+        /*
+         * Consulta la información de una actividad particular.
+         */
         public EntidadActividad consultarActividad(String codigo)
         {
-            /*consulta la información de una actividad particular*/
             return controladoraBDActividades.consultarActividad(codigo);
         }
 
+        /*
+         * Crea una nueva actividad dado un vector con los datos de la misma.
+         */
         public String[] insertarDatos(String descripcion, int estado)
         {
-            /*crea una nueva actividad dado un vector con los datos de la misma*/
             Object[] datosActividad = new Object[3];
             datosActividad[0] = "";
             datosActividad[1] = descripcion;
@@ -36,9 +44,11 @@ namespace ProyectoInventarioOET.Módulo_Actividades
             return controladoraBDActividades.insertarActividad(actividad);
         }
 
+        /*
+         * Crea una nueva actividad dado un vector con los datos de la misma
+         */
         public String[] modificarDatos(EntidadActividad actividadVieja, String descripcionNueva, int estadoNuevo)
         {
-            /*crea una nueva actividad dado un vector con los datos de la misma*/
             Object[] actividadNueva = new Object[3];
             actividadNueva[0] = actividadVieja.Codigo;
             actividadNueva[1] = descripcionNueva;
@@ -48,15 +58,19 @@ namespace ProyectoInventarioOET.Módulo_Actividades
             return controladoraBDActividades.modificarActividad(actividadVieja, actividad);
         }
 
+        /*
+         * Desactiva una actividad de la base de datos
+         */
         public String[] desactivarActividad(EntidadActividad actividad)
         {
-            /*desactiva una actividad de la base de datos*/
             return controladoraBDActividades.desactivarActividad(actividad);
         }
 
+        /*
+         * Consulta la información de todas las bodegas
+         */
         public DataTable consultarActividades()
         {
-            /*consulta la información de todas las bodegas*/
             return controladoraBDActividades.consultarActividades();
         }
     }
