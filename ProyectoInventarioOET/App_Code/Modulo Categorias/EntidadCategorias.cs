34a5dc8c2851758bd874317c8bd99363bb5295d8
Oscar Esquivel <oscar12esquivel@gmail.com>
2015-04-26 21:04:54 -0600

    Iniciada documentación en EntidadCategorias.cs

--- ProyectoInventarioOET/App_Code/Modulo Categorias/EntidadCategorias.cs
+++ ProyectoInventarioOET/App_Code/Modulo Categorias/EntidadCategorias.cs
@@ -5,21 +5,31 @@ using System.Web;
 
 namespace ProyectoInventarioOET.Modulo_Categorias
 {
+    /*
+     * ???
+     */
     public class EntidadCategoria
     {
-        private String nombre;
-        private String descripcion;
-        private int estado; 
-        private object[] datoscategoriaNueva;
-        private string p;
+        //Atributos
+        private String nombre;                  //???
+        private String descripcion;             //???
+        private int estado;                     //???
+        private object[] datoscategoriaNueva; //wtf?
+        private string p; //wtf?
 
-
+        /*
+         * Constructor.
+         */
         public EntidadCategoria(Object[] datos)
         {
             this.nombre = datos[0].ToString();
             this.descripcion = datos[1].ToString();
             this.estado = (int)datos[2];
         }
+
+        /*
+         * Constructor.
+         */
         public EntidadCategoria(String descripcion)
         {
             this.descripcion = descripcion;
@@ -27,17 +37,25 @@ namespace ProyectoInventarioOET.Modulo_Categorias
             this.estado = 1;
         }
 
+        /*
+         * ???
+         */
         public String Nombre
         {
             get { return nombre; }
             set { nombre = value; }
         }
 
+        /*
+         * ???
+         */
         public String Descripcion
         {
             get { return descripcion; }
             set { descripcion = value; }
         }
+
+        //TODO: eliminar este método.
         public string autogenerarCodigo()
         {
 
