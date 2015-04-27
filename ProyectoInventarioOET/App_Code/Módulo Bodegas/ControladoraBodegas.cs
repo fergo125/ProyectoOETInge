dabf78be2b5f578c70e75e4c929ef9dfc53fd00a
Oscar Esquivel <oscar12esquivel@gmail.com>
2015-04-26 20:53:39 -0600

    Documentación iniciada en ControladoraBodegas.cs

--- ProyectoInventarioOET/App_Code/Módulo Bodegas/ControladoraBodegas.cs
+++ ProyectoInventarioOET/App_Code/Módulo Bodegas/ControladoraBodegas.cs
@@ -7,52 +7,69 @@ using System.Data;
 
 namespace ProyectoInventarioOET.Módulo_Bodegas
 {
+    /*
+     * ???
+     */
     public class ControladoraBodegas
     {
-        
-        private ControladoraBDBodegas controladoraBDBodegas;
+        //Atributos
+        private ControladoraBDBodegas controladoraBDBodegas;    //???
 
+        /*
+         * Constructor.
+         */
         public ControladoraBodegas()
         {
             controladoraBDBodegas = new ControladoraBDBodegas();
         }
 
-
+        /*
+         * Consulta la información de una bodega particular.
+         */
         public EntidadBodega consultarBodega(String id)
         {
-            /*consulta la información de una bodega particular*/
             return controladoraBDBodegas.consultarBodega(id);
         }
 
+        /*
+         * Crea una nueva bodega dado un vector con los datos de la misma.
+         */
         public String[] insertarDatos(Object[] datosBodega)
         {
-            /*crea una nueva bodega dado un vector con los datos de la misma*/
             EntidadBodega bodega = new EntidadBodega(datosBodega);
             return controladoraBDBodegas.insertarBodega(bodega);
         }
 
+        /*
+         * Modifica los datos de una bodega particular.
+         */
         public String[] modificarDatos(EntidadBodega bodegaVieja, Object[] datosBodegaNueva)
         {
-            /*modifica los datos de una bodega particular*/
             EntidadBodega bodegaNueva = new EntidadBodega(datosBodegaNueva);
             return controladoraBDBodegas.modificarBodega(bodegaVieja, bodegaNueva);
         }
 
+        /*
+         * Desactiva una bodega de la base de datos.
+         */
         public String[] desactivarBodega(EntidadBodega bodega)
         {
-            /*desactiva una bodega de la base de datos*/
             return controladoraBDBodegas.desactivarBodega(bodega);
         }
 
+        /*
+         * Consulta la información de todas las bodegas.
+         */
         public DataTable consultarBodegas()
         {
-            /*consulta la información de todas las bodegas*/
             return controladoraBDBodegas.consultarBodegas();
         }
 
+        /*
+         * Consulta la información de las bodegas pertenecientes a la estación.
+         */
         public DataTable consultarBodegasDeEstacion(String codigo)
         {
-            /*consulta la información de las bodegas pertenecientes a la estación*/
             return controladoraBDBodegas.consultarBodegasDeEstacion(codigo);
         }
     }
