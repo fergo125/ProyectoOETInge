7865480d8b4af5edb4153ea7e226408dba0d8c86
Oscar Esquivel <oscar12esquivel@gmail.com>
2015-04-26 21:13:16 -0600

    Documentación iniciada en ControladoraBDSeuguridad.

--- ProyectoInventarioOET/App_Code/Módulo Seguridad/ControladoraBDSeguridad.cs
+++ ProyectoInventarioOET/App_Code/Módulo Seguridad/ControladoraBDSeguridad.cs
@@ -5,18 +5,25 @@ using System.Web;
 using System.Data;
 using Oracle.DataAccess.Client; //para conectarse a la base de datos manualmente con strings
 
-/*
- * Controladora de base de datos de seguridad
- * Encargada de obtener información acerca de la seguridad del sistema desde la base de datos
- * Hereda de la controladora de base de datos
- */
 namespace ProyectoInventarioOET.Módulo_Seguridad
 {
+    /*
+     * Controladora de base de datos de seguridad, encargada de obtener información acerca de la seguridad
+     * del sistema desde la base de datos. Hereda de la controladora de base de datos
+     * Comunicación con la Base de Datos.
+     */
     public class ControladoraBDSeguridad : ControladoraBD
     {
-        public ControladoraBDSeguridad() { }
+        /*
+         * Constructor.
+         */
+        public ControladoraBDSeguridad()
+        {
+        }
 
-        // Busca un usuario con un nombre y una password específicos
+        /*
+         * Busca un usuario con un nombre y un password específicos.
+         */
         public EntidadUsuario consultarUsuario(String nombre, String password)
         {
             DataTable resultado = new DataTable();
@@ -40,6 +47,10 @@ namespace ProyectoInventarioOET.Módulo_Seguridad
             return usuario;
         }
 
+        /*
+         * Obtiene el perfil (en texto, no llave) que tiene asignado un usuario usando su código interno.
+         * TODO: obtener la llave también por si se llega a necesitar.
+         */
         public String consultarPerfilUsuario(String codigoUsuario)
         {
             DataTable resultado = new DataTable();
