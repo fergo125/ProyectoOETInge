b6c7bc6e125669bb7b428b78f914a5d55287665a
Oscar Esquivel <oscar12esquivel@gmail.com>
2015-04-26 21:20:15 -0600

    Iniciada documentación de EntidadUsuario.cs

--- ProyectoInventarioOET/App_Code/Módulo Seguridad/EntidadUsuario.cs
+++ ProyectoInventarioOET/App_Code/Módulo Seguridad/EntidadUsuario.cs
@@ -3,26 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-/*
- * Entidad Usuario
- * Encapsulador de información acerca de cuentas de usuario
- */
 namespace ProyectoInventarioOET.Módulo_Seguridad
 {
+    /*
+     * Entidad Usuario, clase encargada de encapsulador la información acerca de cuentas de usuario
+     */
     public class EntidadUsuario
     {
-        private String codigo;
-        private String usuario;
-        private String clave;
-        private DateTime fechaCreacion;
-        private String descripcion;
-        private String idEstacion;
-        private String anfitriona;
-        private String nombre;
-        private int estado;
-        private String perfil;
+        //Atributos
+        private String codigo;              //???
+        private String usuario;             //???
+        private String clave;               //???
+        private String descripcion;         //???
+        private String idEstacion;          //???
+        private String anfitriona;          //???
+        private String nombre;              //???
+        private String perfil;              //???
+        private DateTime fechaCreacion;     //???
+        private int estado;                 //???
 
-        // Constructor
+        /*
+         * Constructor.
+         */
         public EntidadUsuario(Object[] datos)
         {
             this.codigo = datos[0].ToString();
@@ -36,65 +38,94 @@ namespace ProyectoInventarioOET.Módulo_Seguridad
             this.estado = Convert.ToInt32(datos[8].ToString());
         }
 
+        /*
+         * ???
+         */
         public String Codigo
         {
             get { return codigo; }
             set { codigo = value; }
         }
 
+        /*
+         * ???
+         */
         public String Usuario
         {
             get { return usuario; }
             set { usuario = value; }
         }
 
+        /*
+         * ???
+         */
         public String Clave
         {
             get { return clave; }
             set { clave = value; }
         }
 
+        /*
+         * ???
+         */
         public DateTime FechaCreacion
         {
             get { return fechaCreacion; }
             set { fechaCreacion = value; }
         }
 
+        /*
+         * ???
+         */
         public String Descripcion
         {
             get { return descripcion; }
             set { descripcion = value; }
         }
 
+        /*
+         * ???
+         */
         public String IdEstacion
         {
             get { return idEstacion; }
             set { idEstacion = value; }
         }
 
+        /*
+         * ???
+         */
         public String Anfitriona
         {
             get { return anfitriona; }
             set { anfitriona = value; }
         }
 
+        /*
+         * ???
+         */
         public String Nombre
         {
             get { return nombre; }
             set { nombre = value; }
         }
 
+        /*
+         * ???
+         */
         public int Estado
         {
             get { return estado; }
             set { estado = value; }
         }
 
+        /*
+         * ???
+         */
         public String Perfil
         {
             get { return perfil; }
             set { perfil = value; }
         }
-
     }
 }
