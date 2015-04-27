b5ae07796f2ac2fb2585caea38526539f4175557
Oscar Esquivel <oscar12esquivel@gmail.com>
2015-04-26 21:10:44 -0600

    Documentación iniciada en EntidadProductoLocal.cs

--- ProyectoInventarioOET/App_Code/Módulo Productos Locales/EntidadProductoLocal.cs
+++ ProyectoInventarioOET/App_Code/Módulo Productos Locales/EntidadProductoLocal.cs
@@ -5,20 +5,26 @@ using System.Web;
 
 namespace ProyectoInventarioOET.Módulo_Productos_Locales
 {
+    /*
+     * ???
+     */
     public class EntidadProductoLocal
     {
+        //Atributos
+        private String nombre;              //???
+        private String categoria;           //String ???
+        private String unidades;            //???
+        private String codigo;              //???
+        private String codigoDeBarras;      //???
+        private String estacion;            //???
+        private int estado;                 //???
+        private double costoColones;        //???
+        private double costoDolares;        //???, cuyo cálculo se hace en interfaz
+        private String inv_productos;       //Llave primaria autogenerada en la BD
 
-        private String nombre;
-        private String categoria; // es string
-        private String unidades;
-        private String codigo;
-        private String codigoDeBarras;
-        private String estacion;
-        private int estado;
-        private double costoColones;
-        private double costoDolares; //Calculo se hace en interfaz
-        private String inv_productos; //Clave primaria es autogenerada en la BD
-
+        /*
+         * Constructor.
+         */
         public EntidadProductoLocal(Object[] datos)
         {
             this.nombre = datos[0].ToString();
@@ -32,74 +38,102 @@ namespace ProyectoInventarioOET.Módulo_Productos_Locales
             this.costoDolares = Convert.ToDouble(datos[8].ToString());
         }
 
-
+        /*
+         * ???
+         */
         public String Nombre
         {
             get { return nombre; }
             set { nombre = value; }
         }
 
+        /*
+         * ???
+         */
         public String Categoria
         {
             get { return categoria; }
             set { categoria = value; }
         }
+
+        /*
+         * ???
+         */
         public String Unidades
         {
             get { return unidades; }
             set { unidades = value; }
         }
+
+        /*
+         * ???
+         */
         public String Codigo
         {
             get { return codigo; }
             set { codigo = value; }
         }
 
+        /*
+         * ???
+         */
         public String CodigoDeBarras
         {
             get { return codigoDeBarras; }
             set { codigoDeBarras = value; }
         }
 
-
+        /*
+         * ???
+         */
         public String Estacion
         {
             get { return estacion; }
             set { estacion = value; }
         }
 
+        /*
+         * ???
+         */
         public int Estado
         {
             get { return estado; }
             set { estado = value; }
         }
 
+        /*
+         * ???
+         */
         public double CostoColones
         {
             get { return costoColones; }
             set { costoColones = value; }
         }
 
+        /*
+         * ???
+         */
         public double CostoDolares
         {
             get { return costoDolares; }
             set { costoDolares = value; }
         }
 
+        /*
+         * ???
+         */
         public String Inv_Productos
         {
             get { return inv_productos; }
             set { inv_productos = value; }
         }
 
+        //TODO: eliminar este método.
         private String autogenerarCodigo()
         {
             //here's where the magic happens
             //in the meanwhile...
             return "";
         }
-
-
-
     }
 }
