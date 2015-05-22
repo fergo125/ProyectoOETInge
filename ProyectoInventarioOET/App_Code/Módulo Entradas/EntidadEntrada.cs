f3803e59797b1d53ec78254173a0e2b60dede704
fergo125 <ffmm.14@gmail.com>
2015-05-22 09:25:44 -0600

    Se implemento la entidad facturas y se agregaron las clases de entradas

---
+++ ProyectoInventarioOET/App_Code/Módulo Entradas/EntidadEntrada.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoInventarioOET.Módulo_Entradas
+{
+    public class EntidadEntrada
+    {
+
+    }
+}
