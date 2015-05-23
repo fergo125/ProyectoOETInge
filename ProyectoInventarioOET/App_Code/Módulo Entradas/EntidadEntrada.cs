04411d0747ba8af154d0bdd3cd728fc9ed90bff8
alesk8400 <alesk8400@gmail.com>
2015-05-23 16:41:58 -0600

    (Re)Agregando archivos

---
+++ ProyectoInventarioOET/App_Code/Módulo Entradas/EntidadEntrada.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoInventarioOET.Módulo_Entradas
+{
+    public class EntidadEntrada
+    {
+
+    }
+}
