054ef941dc2b295e368781e76c17995e9234464b
alesk8400 <alesk8400@gmail.com>
2015-05-27 18:18:37 -0600

    Boton para agregar a factura nueva

--- ProyectoInventarioOET/FormEntradas.aspx.designer.cs
+++ ProyectoInventarioOET/FormEntradas.aspx.designer.cs
@@ -346,13 +346,13 @@ namespace ProyectoInventarioOET {
         protected global::System.Web.UI.HtmlControls.HtmlInputText Text2;
         
         /// <summary>
-        /// Control Button1.
+        /// Control agregarProducto.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Button agregarProducto;
         
         /// <summary>
         /// Control Div3.
