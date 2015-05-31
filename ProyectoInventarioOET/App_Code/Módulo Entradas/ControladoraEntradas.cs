73358cdce2a61eb5835771274d0e47491615ed9f
alesk8400 <alesk8400@gmail.com>
2015-05-31 00:16:56 -0600

    Avance en documentacion, orden y acciones de manipulacion de productos elegidos.

--- ProyectoInventarioOET/App_Code/Módulo Entradas/ControladoraEntradas.cs
+++ ProyectoInventarioOET/App_Code/Módulo Entradas/ControladoraEntradas.cs
@@ -97,5 +97,10 @@ namespace ProyectoInventarioOET.Modulo_Entradas
         {
             return controladoraBDEntradas.consultarDetalleFactura(idOrdenDeCompra);
         }
+
+        public DataTable consultarProductosEntrada(string id)
+        {
+            return controladoraBDEntradas.consultarProductosEntrada(id);
+        }
     }
 }
