041010b504f3636f39ed57fdffc61ca5513cdc8b
elGranCali <cali2910@gmail.com>
2015-06-10 10:43:48 -0600

    Documentacion carlos lista

--- ProyectoInventarioOET/App_Code/Módulo Traslados/ControladoraBDTraslado.cs
+++ ProyectoInventarioOET/App_Code/Módulo Traslados/ControladoraBDTraslado.cs
@@ -9,11 +9,19 @@ using ProyectoInventarioOET.App_Code.Modulo_Ajustes;
 
 namespace ProyectoInventarioOET.App_Code.Modulo_Traslados
 {
+
+    /*
+     * Clase encargada de la comunicación entre el modulo de traslado y la Base de Datos.
+     */
     public class ControladoraBDTraslado : ControladoraBD
     {
 
 
-        // Funciona
+        /*
+         * Método encargado de consultar todos los traslados de una bodega específica
+         * con los datos listos para ser desplegados por la interfaz, notar que segun el tipo de traslado (entrada i salida)
+         * el SQL cambia
+         */
         public DataTable consultaTraslados(String idBodega, bool entrada)
         {
             String esquema = "Inventarios.";
@@ -23,7 +31,7 @@ namespace ProyectoInventarioOET.App_Code.Modulo_Traslados
             try
             {
                 // Interfaz ocupa 3 cosas TipoMovimiento(Descripcion), Fecha, Usuario(Encargado)
-                // Yo agrego el ID de ajustes para la consulta individual
+                // Se agrega el ID de los traslados para la consulta individual
                 OracleCommand command = conexionBD.CreateCommand();
                 command.CommandText = "SELECT T.ID_TRASLADO, T.FECHA, U.NOMBRE, T.IDBODEGAORIGEN, T.IDBODEGADESTINO, B1.DESCRIPCION, B2.DESCRIPCION, T.ESTADO  "
                    + " FROM " + esquema + "TRASLADOS T, " + esquema + "SEG_USUARIO U, " + esquema + "CAT_BODEGA B1, " + esquema + "CAT_BODEGA B2 "
@@ -41,7 +49,11 @@ namespace ProyectoInventarioOET.App_Code.Modulo_Traslados
             return resultado;
         }
 
-        // LISTO
+
+        /*
+         * Método encargado de consultar un traslado especifico de una bodega específica
+         * los datos incluyen los datos totales del traslado incluyendo los datos desplegables de los productos trasladados 
+         */       
         public DataTable[] consultarTraslado(String idTraslado)
         {
             String esquema = "Inventarios.";
@@ -69,7 +81,10 @@ namespace ProyectoInventarioOET.App_Code.Modulo_Traslados
             return resultado;
         }
 
-        //listo
+        /*
+         * Método auxiliar encargado de consultar los datos desplegables (por ejemplo el nombre del producto en lugar de su ID) 
+         * de los productos trasladados de un ajuste específico
+         */
         private DataTable consultarDetalles(String idTraslado)
         {
             String esquema = "Inventarios.";
@@ -95,7 +110,10 @@ namespace ProyectoInventarioOET.App_Code.Modulo_Traslados
         }
 
 
-
+        /*
+         * Método encargado de insertar los datos ingresados por el usuario, notar que los datos entrantes vienen encapsulados para mayor facilidad para la 
+         * construcción del String SQL
+         */
         public String[] insertarTraslado(EntidadTraslado nuevo)
         {
             String esquema = "Inventarios.";
@@ -110,7 +128,7 @@ namespace ProyectoInventarioOET.App_Code.Modulo_Traslados
                 OracleDataReader reader = command.ExecuteReader();
 
                 foreach (EntidadDetalles detallesProducto in nuevo.Detalles)
-                { // Por cada producto meterlo en el detalles ajustes
+                { // Por cada producto ingresarlo en el detalles traslado
                     insertarDetalle(res[3], detallesProducto);
                     congelarProducto(detallesProducto.IdProductoBodegaOrigen, detallesProducto.Cambio);
                 }
@@ -130,7 +148,9 @@ namespace ProyectoInventarioOET.App_Code.Modulo_Traslados
         }
 
 
-        // Actualiza el inventario local y pone los productos en la columna congelados 
+        /* Método encargado de "congelar" los productos del inventario local añadiendolos en la columna congelados para que su existencia 
+        * no este disponible. 
+        */
         private void congelarProducto(String idProductoBodega, double traslado)
         {
             String esquema = "Inventarios.";
@@ -148,6 +168,11 @@ namespace ProyectoInventarioOET.App_Code.Modulo_Traslados
             }
         }
 
+
+        /* Método encargado de "descongelar" los productos del inventario local moviendolos de la columna congelados a la columna de existencia para que su existencia 
+        * este disponible. Dicha disponibilidad dependera de si se trata de aceptar un traslado (los productos se agregan a la bodega destino) 
+        * o rechazarlo (los productos se retornan a la bodega origen)  
+        */
         private void desCongelarProducto(String idProductoBodegaOrigen, String idProductoBodegaDestino, double traslado, int aceptarTraslado)
         {
             String esquema = "Inventarios.";  
@@ -184,7 +209,10 @@ namespace ProyectoInventarioOET.App_Code.Modulo_Traslados
             catch (OracleException e)  {             }
         }
 
-
+        /*
+         * Método auxiliar encargado de insertar los productos traslados y su respectiva cantidad en la 
+         *  tabla detalles de traslado producto de la relación N:M con la tabla de traslados 
+         */
         private void insertarDetalle(String idTraslado, EntidadDetalles detallesProducto)
         {
             String esquema = "Inventarios.";
@@ -208,6 +236,9 @@ namespace ProyectoInventarioOET.App_Code.Modulo_Traslados
         }
 
 
+        /*
+         * Método encargado de hacer o delegar todos los cambios necesarios cuando se acepta o rechaza un traslado 
+         */
         public String[] modificarTraslado(EntidadTraslado traslado, int estado) {
             String esquema = "Inventarios.";
             String[] res = new String[3];
