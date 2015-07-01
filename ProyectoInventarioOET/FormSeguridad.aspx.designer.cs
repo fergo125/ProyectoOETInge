dd546f2620a69e7bfd9f917bc114ed8f314b379f
BlopaSc <blopa.sauma@hotmail.com>
2015-07-01 10:37:28 -0600

    Busquda con fechas.

--- ProyectoInventarioOET/FormSeguridad.aspx.designer.cs
+++ ProyectoInventarioOET/FormSeguridad.aspx.designer.cs
@@ -253,7 +253,7 @@ namespace ProyectoInventarioOET {
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText inputPassword;
+        protected global::System.Web.UI.HtmlControls.HtmlInputPassword inputPassword;
         
         /// <summary>
         /// Control inputPasswordConfirm.
